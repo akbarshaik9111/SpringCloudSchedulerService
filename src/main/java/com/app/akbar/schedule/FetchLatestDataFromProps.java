de19935fbe2ff4073c6c80fef6548861092c3e35
Akbar <akbarshaik9111@gmail.com>
2023-09-15 14:01:16 +0530

    Akbar

---
+++ src/main/java/com/app/akbar/schedule/FetchLatestDataFromProps.java
@@ -0,0 +1,25 @@
+package com.app.akbar.schedule;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class FetchLatestDataFromProps {
+	
+	@Scheduled(cron = "10 * * * * *")
+	public void fetch() {
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> entity = new HttpEntity<>("{}", headers);
+		
+		RestTemplate rt = new RestTemplate();
+		String url = "http://192.168.1.7:8082/actuator/refresh";
+		rt.postForEntity(url, entity, String.class);
+	}
+
+}
